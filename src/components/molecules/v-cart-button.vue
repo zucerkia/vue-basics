77ae2d0cffaca755116a7c305c2616ec6bf9acee
Julian Sucerquia <andresucerquia@gmail.com>
2023-07-03 16:39:01 -0500

    create components

---
+++ src/components/molecules/v-cart-button.vue
@@ -0,0 +1,46 @@
+<template>
+  <button class="cart-button">
+    <Icon class="cart-icon" name="fa-cart-shopping" size="18px" />
+    <span class="cart-badge">{{ badge }}</span>
+  </button>
+</template>
+
+<script>
+import Icon from "@/components/atoms/v-icon.vue";
+export default {
+  components: {
+    Icon,
+  },
+  data() {
+    return {
+      badge: 0,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.cart-button {
+  position: relative;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+}
+
+.cart-icon {
+  color: var(--white);
+}
+
+.cart-badge {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  display: grid;
+  place-content: center;
+  top: -10px;
+  right: -10px;
+  background: var(--white);
+  border-radius: 50%;
+  border: 1px solid var(--primary);
+}
+</style>
---
+++ src/components/molecules/v-nav.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="header-nav">
+    <ul class="header-links">
+      <li class="link">
+        <a href="#">Sobre nosotros</a>
+      </li>
+      <li class="link">
+        <a href="#">Contactenos</a>
+      </li>
+    </ul>
+    <CartButton />
+    <Button @handleClick="print('iniciar sesion')"> Iniciar sesion </Button>
+    <Button @handleClick="print('registrase')" type="secondary">
+      Registrate
+    </Button>
+  </nav>
+</template>
+
+<script>
+import Button from "@/components/atoms/v-button.vue";
+import CartButton from "@/components/molecules/v-cart-button.vue";
+
+export default {
+  components: {
+    Button,
+    CartButton,
+  },
+  methods: {
+    print(e) {
+      console.log(e);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.link {
+  color: var(--white);
+  font-weight: 600;
+}
+
+.header-nav {
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+}
+
+.header-links {
+  margin: 0;
+  display: flex;
+  list-style: none;
+  gap: 1rem;
+  padding: 0;
+}
+</style>
